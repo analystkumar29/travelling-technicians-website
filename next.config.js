9e7f9a4db50523699d82d9f0e280f1294a68c402
Manoj Kumar <analystkumar29@gmail.com>
2025-04-24 16:55:26 -0700

    Fix SendGrid email configuration with dynamic template

--- next.config.js
+++ next.config.js
@@ -1,1 +1,1 @@
-/** @type {import("next").NextConfig} */ const nextConfig = { reactStrictMode: true, images: { domains: ["images.unsplash.com"], }, trailingSlash: true }; module.exports = nextConfig;
+/** @type {import('next').NextConfig} */ const nextConfig = { reactStrictMode: true, images: { domains: ['images.unsplash.com'] } }; module.exports = nextConfig;
