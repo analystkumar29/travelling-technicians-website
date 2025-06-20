f12f6b68f118bece402f83c518fb606edd31bc67
Manoj Kumar <analystkumar29@gmail.com>
2025-06-19 21:42:24 -0700

    Fix syntax error in admin bookings - remove duplicate closing parentheses

--- src/pages/admin/bookings.tsx
+++ src/pages/admin/bookings.tsx
@@ -684,7 +684,6 @@ export default function AdminBookings() {
                       )}
                     </div>
                   </li>
-              ))}
                 ))
               )}
             </ul>
