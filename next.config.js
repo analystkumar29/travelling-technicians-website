7ac6cc877ce1a314249ae703d829926d9f5deb01
Manoj Kumar <analystkumar29@gmail.com>
2025-09-16 07:18:09 +0530

    feat: add domain redirects and security headers

--- next.config.js
+++ next.config.js
@@ -5,6 +5,47 @@ module.exports = (phase, { defaultConfig }) => {
   const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    // Domain redirects and headers
+    async redirects() {
+      return [
+        {
+          source: '/',
+          has: [
+            {
+              type: 'host',
+              value: 'travelling-technicians.ca',
+            },
+          ],
+          destination: 'https://www.travelling-technicians.ca',
+          permanent: true,
+        },
+      ];
+    },
+    async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            {
+              key: 'X-DNS-Prefetch-Control',
+              value: 'on'
+            },
+            {
+              key: 'Strict-Transport-Security',
+              value: 'max-age=63072000; includeSubDomains; preload'
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff'
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'strict-origin-when-cross-origin'
+            }
+          ],
+        },
+      ];
+    },
     // Disable Fast Refresh in development
     webpack: (config, { dev, isServer }) => {
       if (dev) {
